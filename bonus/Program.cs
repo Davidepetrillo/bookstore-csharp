9bbc9464c6a6508e697bf2767fc8cd4e627c7015
davidepetrillo <davide96petrillo@gmail.com>
2022-04-04 15:54:25 +0200

    commit 3 - eliminata la parte del commento

--- bonus/Program.cs
+++ bonus/Program.cs
@@ -59,26 +59,5 @@ Console.WriteLine($"Copertina flessibile disponibile: {richiestaCopertinaFlessib
 
 
 
-/*
-int numeroDellePagine = 431;
-Console.WriteLine($"Numero delle pagine: {numeroDellePagine} pagine");
-int pesoDelLibro = 660;
-Console.WriteLine($"Peso del libro: {pesoDelLibro}g");
-double larghezzaDelLibro = 17.91;
-double lunghezzaDelLibro = 2.54;
-double profonditaDelLibro = 23.5;
-Console.WriteLine($"Dimensioni del libro: {larghezzaDelLibro} x {lunghezzaDelLibro} x {profonditaDelLibro} cm");
-Console.WriteLine("");
-Console.WriteLine("Informazioni Amazon:");
-int numeroDiRecensioni = 3307;
-Console.WriteLine($"Numero di recensioni: {numeroDiRecensioni} recensioni");
-double valutazioneMedia = 4.7;
-Console.WriteLine($"Valutazione media: {valutazioneMedia} stelle su 5");
-string kindle = "si";
-Console.WriteLine($"Kindle disponibile: {kindle}");
-string copertinaFlessibile = "si";
-Console.WriteLine($"Copertina flessibile disponibile: {copertinaFlessibile}");
-*/
-
 
 
