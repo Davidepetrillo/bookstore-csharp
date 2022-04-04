ced92f51bb174a7aefa33e6a21be286ffd841492
davidepetrillo <davide96petrillo@gmail.com>
2022-04-04 14:57:53 +0200

    Add project files.

---
+++ Bookstore Csharp/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("--- IL LIBRO DI OGGI: CLEAN CODE di Robert C.Martin ---");
+Console.WriteLine("");
+Console.WriteLine("Informazioni generiche:");
+long isbn = 9780132350884;
+Console.WriteLine($"ISBN: {isbn}");
+int numeroDellePagine = 431;
+Console.WriteLine($"Numero delle pagine: {numeroDellePagine} pagine");
+int pesoDelLibro = 660;
+Console.WriteLine($"Peso del libro: {pesoDelLibro}g");
+double larghezzaDelLibro = 17.91;
+double lunghezzaDelLibro = 2.54;
+double profonditàDelLibro = 23.5;
+Console.WriteLine($"Dimensioni del libro: {larghezzaDelLibro} x {lunghezzaDelLibro} x {profonditàDelLibro} cm");
+Console.WriteLine("");
+Console.WriteLine("Informazioni Amazon:");
+int numeroDiRecensioni = 3307;
+Console.WriteLine($"Numero di recensioni: {numeroDiRecensioni} recensioni");
+double valutazioneMedia = 4.7;
+Console.WriteLine($"Valutazione media: {valutazioneMedia} stelle su 5");
+string kindle = "si";
+Console.WriteLine($"Kindle disponibile: {kindle}");
+string copertinaFlessibile = "si";
+Console.WriteLine($"Copertina flessibile disponibile: {copertinaFlessibile}");
+
+
+
+
